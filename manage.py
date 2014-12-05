63c9a99e8f99ecdb3654087094c0f7bace7eba2e
Janny Kinende <janny@haiinteraktiv.no>
2014-12-05 16:54:00 +0100

    commit

--- manage.py
+++ manage.py
@@ -3,11 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-
-	CurDir = os.path.dirname(os.path.abspath(__file__))
-	ProjectDir = os.path.join(CurDir, "controlpanel")
-	sys.path += [ProjectDir]
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "controlpanel.settings")
 
     from django.core.management import execute_from_command_line
