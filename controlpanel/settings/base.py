4d718ae094d46d249b974b4e12298f12337751c3
Janny Kinende <janny@haiinteraktiv.no>
2015-03-26 16:39:11 +0100

    small fix

--- controlpanel/settings/base.py
+++ controlpanel/settings/base.py
@@ -66,16 +66,24 @@ WSGI_APPLICATION = 'controlpanel.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+#DATABASES = {
+    #'default': {
+     #   'ENGINE': 'django.db.backends.postgresql_psycopg2',
+      #  'NAME': 'REPLACE_WITH_DB_NAME',
+       # 'USER': 'REPLACE_WITH_DB_USER',
+       # 'PASSWORD': 'REPLACE_WITH_USER_PASSWORD',
+       # 'HOST': 'localhost',
+       # 'PORT': '5432',
+ #   }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'REPLACE_WITH_DB_NAME',
-        'USER': 'REPLACE_WITH_DB_USER',
-        'PASSWORD': 'REPLACE_WITH_USER_PASSWORD',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': root('..', 'db.sqlite3')
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -113,7 +121,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 MEDIA_ROOT = root('..', 'uploads')
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 STATIC_ROOT = root('..', 'staticfiles')
 ADMIN_MEDIA_PREFIX = '/static/admin'
 
