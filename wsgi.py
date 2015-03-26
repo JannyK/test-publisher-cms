84cc932e98031db53a416fb2f7caec9b2986a376
Janny Kinende <janny@haiinteraktiv.no>
2015-03-26 17:26:23 +0100

    added wsgi for apache deployment

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,6 @@ import site
 
 sys.stdout = sys.stderr
 
-site.addsitedir('~/.virtualenvs/cms/local/lib/python2.7/site-packages')
-
-sys.path.insert(0, "")
 sys.path.insert(0, "/home/ubuntu")
 sys.path.insert(0, "/home/ubuntu/test-publisher-cms")
 os.environ['DJANGO_SETTINGS_MODULE'] = 'controlpanel.settings'
