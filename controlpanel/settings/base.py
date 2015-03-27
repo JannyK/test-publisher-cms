a89646edbd93abb5c7b2295bdebc61e1942e6fd4
Janny Kinende <janny@haiinteraktiv.no>
2015-03-27 08:39:26 +0100

    chnaged db settings

--- controlpanel/settings/base.py
+++ controlpanel/settings/base.py
@@ -66,23 +66,23 @@ WSGI_APPLICATION = 'controlpanel.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-#DATABASES = {
-    #'default': {
-     #   'ENGINE': 'django.db.backends.postgresql_psycopg2',
-      #  'NAME': 'REPLACE_WITH_DB_NAME',
-       # 'USER': 'REPLACE_WITH_DB_USER',
-       # 'PASSWORD': 'REPLACE_WITH_USER_PASSWORD',
-       # 'HOST': 'localhost',
-       # 'PORT': '5432',
- #   }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': root('..', 'db.sqlite3')
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cmsdb',
+        'USER': 'ubuntu',
+        'PASSWORD': 'Hereinthewall2508',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': root('..', 'db.sqlite3')
+   # }
+#}
 
 
 # Internationalization
